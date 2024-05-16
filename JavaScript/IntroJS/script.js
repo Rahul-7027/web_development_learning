5785909644fd5df200b8ca5c8f404beb2e940aea
rahul-7027 <rahulbhati0202@gmail.com>
2024-05-16 22:00:58 +0530

    JavaScript Start

---
+++ JavaScript/IntroJS/script.js
@@ -0,0 +1,19 @@
+console.log("app is running")
+
+
+// var a=prompt("Enter Your Number ?");
+// console.log(a)
+
+var isTrue=confirm("Are you want to sure you want to leave this website");
+
+if(isTrue){
+    console.log("Blast")
+}
+else
+{
+    console.log("Not Blast")
+}
+
+document.title="Hello javascript";
+
+document.body.style.backgroundColor="red";

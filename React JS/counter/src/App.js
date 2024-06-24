ebdf249974ed4c4137f17fa1dab12a378ee3497d
rahul-7027 <rahulbhati0202@gmail.com>
2024-06-24 11:44:04 +0530

    react start

---
+++ React JS/counter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counte from './components/Counte';
+
+function App() {
+  return (
+    <div className="App">
+      <Counte/>
+    </div>
+  );
+}
+
+export default App;

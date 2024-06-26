13ee200b228fe15b426ea040df06927865f45d1c
rahul-7027 <rahulbhati0202@gmail.com>
2024-06-26 21:33:55 +0530

    excerise18 display card for fetch data uing the api

---
+++ React JS/excersice18displaydata/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Data from './components/Display/Data';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello World</h3>
+      <Data/>
+    </div>
+  );
+}
+
+export default App;

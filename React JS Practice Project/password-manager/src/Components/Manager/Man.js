922368bf89c65ef26f671b4b968d97f5f4c68eaf
rahul-7027 <rahulbhati0202@gmail.com>
2024-07-01 23:33:09 +0530

    updated

--- React JS Practice Project/password-manager/src/Components/Manager/Man.js
+++ React JS Practice Project/password-manager/src/Components/Manager/Man.js
@@ -96,7 +96,7 @@ const Man = () => {
                                 return (
                                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                           <a href= {item.site} target='_blank'>{item.site}</a>
+                                           <a href= {item.site} target='_blank'>{item.site}</a>   
                                         </th>
                                         <td className="px-6 py-4">{item.username}</td>
                                         <td className="px-6 py-4">{item.password} </td>
